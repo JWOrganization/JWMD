8dd407f74c6b06acef16c92894a6c65a5d348234
apple <apple@apples-iMac.lan>
2017-04-17 17:15:41 +0800

    Add project

---
+++ Maldives/Contents/BaseVC/MDGuideView.m
@@ -0,0 +1,26 @@
+//
+//  MDGuideView.m
+//  Maldives
+//
+//  Created by Tian Wei You on 16/8/8.
+//  Copyright © 2016年 Shanghai DuRui Information Technology Company. All rights reserved.
+//
+
+#import "MDGuideView.h"
+
+@implementation MDGuideView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)backButton:(UIButton *)sender {
+    [self removeFromSuperview];
+}
+
+@end
